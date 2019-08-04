6ade7e1c1933fc3163667d8c4c8181c90954483d
dimasiklrnd <dimasiklrnd@gmail.com>
2019-08-05 02:28:40 +0300

    44

--- robot-tasks-master/task_13.py
+++ robot-tasks-master/task_13.py
@@ -5,7 +5,28 @@ from pyrob.api import *
 
 @task
 def task_8_10():
-    pass
+    while not wall_is_on_the_right():
+        wall()
+        move_right()
+    wall()
+
+
+def wall():
+    if not wall_is_above() and wall_is_beneath():
+        move_up()
+        fill_cell()
+        move_down()
+    elif wall_is_above() and not wall_is_beneath():
+        move_down()
+        fill_cell()
+        move_up()
+    elif not wall_is_above() and not wall_is_beneath():
+        move_down()
+        fill_cell()
+        move_up()
+        move_up()
+        fill_cell()
+        move_down()
 
 
 if __name__ == '__main__':
--- robot-tasks-master/task_4.py
+++ robot-tasks-master/task_4.py
@@ -5,7 +5,14 @@ from pyrob.api import *
 
 @task
 def task_3_3():
-    pass
+    if wall_is_on_the_right() and wall_is_above() and wall_is_beneath():
+        move_left()
+    elif wall_is_on_the_right() and wall_is_above() and wall_is_on_the_left():
+        move_down()
+    elif wall_is_beneath() and wall_is_above() and wall_is_on_the_left():
+        move_right()
+    else:
+        move_up()
 
 
 if __name__ == '__main__':
