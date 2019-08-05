9208dd57f46243097b667ff7382ed7ceda46fd2d
dimasiklrnd <dimasiklrnd@gmail.com>
2019-08-05 23:58:18 +0300

    45

--- robot-tasks-master/task_26.py
+++ robot-tasks-master/task_26.py
@@ -19,15 +19,37 @@ def make_cross():
     move_up()
 
 
-@task(delay=0.02)
-def task_2_4():
+def prohod_right():
     i = 9
     while i > 0:
         make_cross()
         move_right(n=4)
         i -= 1
-
     make_cross()
+
+
+def prohod_left():
+    move_down(n=4)
+    i = 9
+    while i > 0:
+        make_cross()
+        move_left(n=4)
+        i -= 1
+    make_cross()
+
+
+@task(delay=0.02)
+def task_2_4():
+
+    for i in range(2):
+        i = prohod_right()
+        i = prohod_left()
+        i = move_down(n=4)
+
+    prohod_right()
+
+    while not wall_is_on_the_left():
+        move_left()
 
 
 if __name__ == '__main__':
