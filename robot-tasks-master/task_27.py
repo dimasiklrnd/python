df2ac2edac05ba819908431b18eae760ad29f00d
dimasiklrnd <dimasiklrnd@gmail.com>
2019-08-06 02:00:00 +0300

    456

--- robot-tasks-master/task_27.py
+++ robot-tasks-master/task_27.py
@@ -5,7 +5,21 @@ from pyrob.api import *
 
 @task
 def task_7_5():
-    pass
+
+    move_right()
+    fill_cell()
+
+    n = m = 0
+
+    while not wall_is_on_the_right():
+        if n < m:
+            n += 1
+            move_right()
+        else:
+            n = 0
+            m += 1
+            move_right()
+            fill_cell()
 
 
 if __name__ == '__main__':
--- robot-tasks-master/task_28.py
+++ robot-tasks-master/task_28.py
@@ -5,7 +5,14 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    n = 0
+    while n < 5:
+        if cell_is_filled():
+            n += 1
+            if n != 5:
+                move_right()
+        elif not cell_is_filled():
+            move_right()
 
 
 if __name__ == '__main__':
