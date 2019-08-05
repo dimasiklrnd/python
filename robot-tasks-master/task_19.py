cc767cfd2c216d6f8aab4ec2d6f4662727a99a8d
dimasiklrnd <dimasiklrnd@gmail.com>
2019-08-05 19:22:31 +0300

    555

--- robot-tasks-master/task_19.py
+++ robot-tasks-master/task_19.py
@@ -20,12 +20,6 @@ def task_8_29():
         if wall_is_on_the_left() and wall_is_above() and wall_is_beneath():
             move_right(n=8)
 
-        '''if wall_is_on_the_right() and wall_is_above():
-            while not wall_is_on_the_left() and not wall_is_above():
-                move_left()
-            while not wall_is_above():
-                move_up()'''
-
 
 if __name__ == '__main__':
     run_tasks()
--- robot-tasks-master/task_20.py
+++ robot-tasks-master/task_20.py
@@ -5,7 +5,37 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_3():
-    pass
+    for i in range(6):
+        i = cicle()
+
+    move_right()
+
+
+def cicle_right():
+    while not wall_is_on_the_right():
+        move_right()
+        while not wall_is_on_the_right():
+            fill_cell()
+            move_right()
+
+
+def down():
+    move_down()
+
+
+def cicle_left():
+    while not wall_is_on_the_left():
+        move_left()
+        while not wall_is_on_the_left():
+            fill_cell()
+            move_left()
+
+
+def cicle():
+    cicle_right()
+    down()
+    cicle_left()
+    down()
 
 
 if __name__ == '__main__':
