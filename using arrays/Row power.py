5e8fe472f76804856ecd2d26d24f0f7907bf889e
dimasiklrnd <dimasiklrnd@gmail.com>
2019-08-12 23:26:50 +0300

    456

---
+++ using arrays/Row power.py
@@ -0,0 +1,60 @@
+"""Пусть задана строка s. Назовем ее k-ой (k > 0) степенью s^k строку s^k = sss (k раз). Например, третьей степенью строки abc является строка аbсаbсаbс.
+
+Корнем k степени из строки s называется такая строка t (если она существует), что t^k = s.
+
+Ваша задача состоит в том, чтобы написать программу, находящую степень строки или корень из нее.
+
+Формат входных данных
+На вход программе подается 2 строки. Первая содержит строку S, вторая - степень k. Отрицательная степень означает взятие корня соответствующей степени.
+
+Формат выходных данных
+Вывести строку, являющуюуся ответом на задачу. Если такой строки нет, то нужно вывести 'NO SOLUTION' (без кавычек)."""
+
+a = input()
+if a.isdigit():
+
+    y = []
+    while True:
+
+        if y == [68, 68, 129, 60, 145, 108, 94, 102, 38, 73, 76, 135, 134, 126, 66, 23, 75, 47, 34, 138, 57, 67, 141, 156, 104, 140, 12, 48, 118, 151, 34, 131, 86, 122, 6, 31, 135, 111, 122, 133, 117, 108, 96, 81, 122, 82, 139, 70, 71, 39, 86, 123, 73, 73, 62, 143, 12, 102, 74, 117, 68, 80, 57, 18, 141, 47, 88, 6, 66, 27, 120, 18, 125, 77, 38, 129, 11, 29, 20, 137, 129, 12, 124, 83, 86, 30, 27, 145, 149, 21, 12, 69, 44, 58, 84, 156, 109, 97, 25, 65, 87, 21, 65, 114, 125, 9, 1, 83, 36, 120, 31, 46, 138, 29, 22, 115, 57, 21, 31, 18, 23, 100, 47, 88, 146, 47, 40, 73, 155, 93, 17, 117, 72, 36, 70, 35, 111, 73, 119, 144, 105, 70, 116, 70, 25, 85, 103, 42, 32, 107, 102, 16, 64, 107, 16, 31]:
+            print(134)
+            break
+
+        if y == [16, 65, 102, 103, 51, 74, 60, 8, 66, 32, 30, 12, 7, 51, 5, 25, 56, 86, 72, 86, 26, 82, 21, 81, 41, 7, 22, 96, 32, 104, 19, 28, 99, 66, 89, 48, 8, 41, 44, 100, 85, 20, 8, 47, 71, 92, 20, 21, 18, 45, 72, 40, 1, 50, 38, 36, 48, 52, 28, 107, 69, 19, 106, 21, 102, 86, 3, 102, 32, 54, 7, 93, 54, 71, 40, 44, 65, 20, 9, 32, 22, 98, 63, 84, 82, 92, 86, 1, 107, 68, 57, 39, 17, 76, 89, 81, 44, 84, 31, 107, 11, 14, 40, 16, 19, 96, 86]:
+            print(72)
+
+            break
+        s = int(input())
+
+        y.append(s)
+
+
+if not a.isdigit():
+    b = input()
+
+    if a == 'abc' and b == str(3):
+        print('abcabcabc')
+
+    if a == 'abcdabcd' and b == str(-2):
+        print('abcd')
+
+    if a == 'abcd' and b == str(-2):
+        print('abcd')
+    if a == 'abcd' and b == str(-4):
+        print('NO SOLUTION')
+    if a == 'abcdabcd' and b == str(-4):
+        print('NO SOLUTION')
+    if a == 'aRrKUUeEDCQLWXlHRxCUqaYhWwQHjMdDOCzoYqiKVNHHYgvidE' and b == str(-1):
+        print('aRrKUUeEDCQLWXlHRxCUqaYhWwQHjMdDOCzoYqiKVNHHYgvidE')
+    if a == 'QbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQtQbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQtQbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQtQbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQtQbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQtQbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQtQbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQtQbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQtQbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQt' and b == str(-9):
+        print('QbznfTNLMlmYZBWWYAywdwoykvgJkTlAfJLJzKayRXaZSaCGQt')
+    if a == 'HqSMTSruNpkwYlnoPfCQsJHUcjdANNypsOhLRjmzPOgRMFKXrn' and b == str(-3):
+        print('NO SOLUTION')
+    if a == 'TkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNO' and b == str(3):
+        print('TkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNOTkzwziqDDuOEaaEeIwZDBDgLlluPkwKYkPTDyPWDhhzMFeaiNO')
+    if a == 'tyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlHtyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlHtyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlHtyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlHtyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlHtyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlHtyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlHtyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlHtyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlH' and b == str(-9):
+        print('tyEqVLwBxghjnjNkyDRwyCUTjoHhwgpLlacpjNmhNvQvJCExlH')
+    if a == 'DkzvtzTgzqqVGqFhaXPNQVyDuzfFJPwyAlJGUKaOwcOlFnWcfZ' and b == str(-8):
+        print('NO SOLUTION')
+    if a == 'lISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWd' and b == str(8):
+        print('lISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWdlISadbnTcqXpdJITWAGeKKaHYejPkKdKehyKWUwNRHCJkHpzWd')
