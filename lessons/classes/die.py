bca63b9a4a86e2b077449e3b1da4cb6685154678
dimasiklrnd <dimasik.rnd@mail.ru>
2019-08-18 23:03:48 +0300

    i

--- lessons/classes/die.py
+++ lessons/classes/die.py
@@ -7,7 +7,9 @@ class Die():
     def __init__(self, sides=6):
         self.sides = sides
 
-    def roll_die(self):
+    def roll_die(self, a, b):
         """иммтация броска кубика"""
-        self.sides = randint(1, 6)
+        self.a=a
+        self.b = b
+        self.sides = randint(self.a, self.b)
         return self.sides
--- lessons/classes/my_die.py
+++ lessons/classes/my_die.py
@@ -1,6 +1,19 @@
 from die import Die
 
-die1 = Die()
+die1 = Die()  # шестигранный кубик
 for i in range(11):
+    die1.roll_die(1, 6)
     print(die1.sides, end=', ')
-    die1.roll_die()
+
+
+print('')
+die2 = Die()  # десятигранный
+for i in range(11):
+    die1.roll_die(1, 10)
+    print(die1.sides, end=', ')
+
+print('')
+die3 = Die()  # двадцатигранный
+for i in range(11):
+    die1.roll_die(1, 20)
+    print(die1.sides, end=', ')
