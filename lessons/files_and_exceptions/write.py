25096299660dce470d791c44de536b6d2f3ceb0a
dimasiklrnd <dimasik.rnd@mail.ru>
2019-08-19 22:15:51 +0300

    олол

--- lessons/files_and_exceptions/write.py
+++ lessons/files_and_exceptions/write.py
@@ -3,5 +3,5 @@ file_write = 'lessons/files_and_exceptions/programming.txt'
 with open(file_write, 'w') as file_object:
     file_object.write('Мне нравится программировать')
 
-with open(file_write, 'a') as file_object:# атрибут 'a' добавляет в файл не перезаписывая его
+with open(file_write, 'a') as file_object:# атрибут 'a' добавляет в файл не перезаписывая его, 'r+' как чтение так и запись
     file_object.write('\nМне нравится программировать!')
