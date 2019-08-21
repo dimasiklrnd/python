90b86176ddc8bf1a2576091d64e7da6b3e73ff5c
dimasiklrnd <dimasik.rnd@mail.ru>
2019-08-21 21:11:38 +0300

    ui

--- lessons/files_and_exceptions/favorite_number.py
+++ lessons/files_and_exceptions/favorite_number.py
@@ -3,5 +3,25 @@
 
 import json
 
-fav_num = input('Введите ваше любимое число: ')
 
+def favorite_num():
+    name = input("Ваше имя? " )
+    filename = 'lessons/files_and_exceptions/fav_num'+'_'+str(name)+'.json'
+    try:
+        with open(filename) as f_obj:
+            fn = json.load(f_obj)
+        num = fn[1]
+        print('Я знаю ваше любимое число! Это: ' + num)
+
+    except FileNotFoundError:
+        print('\tЯ не знаю вас')
+        fav_num = input('Введите ваше любимое число: ')
+        list_fn=[]
+        list_fn.append(name)
+        list_fn.append(fav_num)
+        with open(filename, 'w') as f_obj:
+            json.dump(list_fn,f_obj)
+        print('\tприходите в следующий раз и я угадаю твое число')
+
+
+favorite_num()
