9c6d7b457a6193f2a3236721f99eec4d8e275cb4
dimasiklrnd <dimasik.rnd@mail.ru>
2019-08-21 22:43:54 +0300

    jkn

---
+++ lessons/projects/alien_invasion/alien_invasion.py
@@ -0,0 +1,20 @@
+import sys
+import pygame
+
+
+def run_game():
+    # Инициализирует игру и создает объект экрана.
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 600))
+    pygame.display.set_caption("Alien Invasion")
+    # Запуск основного цикла игры.
+    while True:
+        # Отслеживание событий клавиатуры и мыши.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
+
+
+run_game()
