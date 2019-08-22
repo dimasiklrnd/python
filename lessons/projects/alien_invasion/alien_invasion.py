e6ea6b66508eeb3403503923da051f6546a9ab55
dimasiklrnd <dimasik.rnd@mail.ru>
2019-08-22 19:34:08 +0300

    vn

--- lessons/projects/alien_invasion/alien_invasion.py
+++ lessons/projects/alien_invasion/alien_invasion.py
@@ -1,11 +1,14 @@
 import sys
 import pygame
+from settings import Settings
 
 
 def run_game():
     # Инициализирует игру и создает объект экрана.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 600))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     # Запуск основного цикла игры.
     while True:
@@ -13,6 +16,8 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        # При каждом проходе цикла перерисовывается экран
+        screen.fill(ai_settings.bg_color)  # Назначение цвета фона
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
 
