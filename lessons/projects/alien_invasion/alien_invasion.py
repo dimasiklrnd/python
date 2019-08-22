05c1976c0f625514aa89545a5d8c17d262d56dc6
dimasiklrnd <dimasik.rnd@mail.ru>
2019-08-22 21:05:53 +0300

    hjh

--- lessons/projects/alien_invasion/alien_invasion.py
+++ lessons/projects/alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
-import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -10,16 +11,14 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    # Создание корабля
+    ship = Ship(ai_settings, screen)
     # Запуск основного цикла игры.
     while True:
         # Отслеживание событий клавиатуры и мыши.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # При каждом проходе цикла перерисовывается экран
-        screen.fill(ai_settings.bg_color)  # Назначение цвета фона
-        # Отображение последнего прорисованного экрана.
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
