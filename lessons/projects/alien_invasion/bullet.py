750b476b613173d32cd70621b0fac09566010c89
dimasiklrnd <dimasiklrnd@gmail.com>
2019-08-22 23:12:24 +0300

    4645

--- lessons/projects/alien_invasion/bullet.py
+++ lessons/projects/alien_invasion/bullet.py
@@ -10,7 +10,8 @@ class Bullet(Sprite):
         super(Bullet, self).__init__()
         self.screen = screen
         # Создание пули в позиции (0,0) и назначение правильной позиции.
-        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.rect = pygame.Rect(
+            0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
         # Позиция пули хранится в вещественном формате.
