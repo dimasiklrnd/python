883112b78dce3f32b9c38383b9bae2d68eecbee3
dimasiklrnd <dimasiklrnd@gmail.com>
2019-08-28 10:04:19 +0300

    l;

---
+++ Creating_module_installation_files/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',  # Аргумент name определяет имя дистрибутива
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='HF Python 2e',
+    author_email='hfpy2e@gmail.com',
+    url='headfirstlabs.com',
+    py_modules=['vsearch'],  # Спиисок файлов, которые нужно включить в пакет
+)
