db88dcab9bcbcc8aa56841ae49a032315b8249d7
dimasiklrnd <dimasiklrnd@gmail.com>
2019-08-31 11:59:06 +0300

    456

--- flask/simple_webapp.py
+++ flask/simple_webapp.py
@@ -1,22 +1,43 @@
 from flask import Flask, session
+from decorator_checker import check_logged_in
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def hello() -> str:
+    return 'Привет из простого веб-приложения.'
+
+
+@app.route('/page1')
+@check_logged_in
+def page1() -> str:
+    return 'This is page 1.'
+
+
+@app.route('/page2')
+@check_logged_in
+def page2() -> str:
+    return 'This is page 2.'
+
+
+@app.route('/page3')
+@check_logged_in
+def page3() -> str:
+    return 'This is page 3.'
+
 
 @app.route('/login')
 def do_login() -> str:
     session['logged_in'] = True
-    return 'You are now logged in.'
+    return 'Вы вошли в систему.'
+
 
 @app.route('/logout')
 def do_logout() -> str:
     session.pop('logged_in')
-    return 'You are now logged out.'
+    return 'Вы вышли из системы.'
 
-@app.route('/status')
-def check_status() -> str:
-    if 'logged_in' in session:
-        return 'You are currently logged in.'
-    return 'You are NOT logged in.'
 
 app.secret_key = 'YouWillNeverGuessMySecretKey'
 
