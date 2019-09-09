0ba031169c47271acf6fb4757f5dcdb2cf9497de
dimasiklrnd <dimasiklrnd@gmail.com>
2019-09-09 12:10:21 +0300

    222

--- practic/Xs_and_Os_Referee.py
+++ practic/Xs_and_Os_Referee.py
@@ -29,8 +29,26 @@ all(len(row) == 3 for row in game_result)'''
 from typing import List
 
 
-def checkio(game_result: List[str]) -> str:
-    return "D" or "X" or "O"
+def checkio(g: List[str]) -> str:
+
+    if g[0][0] == g[0][1] == g[0][2] and g[0][0] != '.':
+        return g[0][0]
+    elif g[0][0] == g[1][1] == g[2][2] and g[0][0] != '.':
+        return g[0][0]
+    elif g[0][0] == g[1][0] == g[2][0] and g[0][0] != '.':
+        return g[0][0]
+    elif g[0][1] == g[1][1] == g[2][1] and g[0][1] != '.':
+        return g[0][1]
+    elif g[0][2] == g[1][2] == g[2][2] and g[0][2] != '.':
+        return g[0][2]
+    elif g[0][2] == g[1][1] == g[2][0] and g[0][2] != '.':
+        return g[0][2]
+    elif g[1][0] == g[1][1] == g[1][2] and g[1][0] != '.':
+        return g[1][0]
+    elif g[2][0] == g[2][1] == g[2][2] and g[2][0] != '.':
+        return g[2][0]
+    else:
+        return "D"
 
 
 if __name__ == '__main__':
@@ -40,6 +58,14 @@ if __name__ == '__main__':
                    "XOO"]))
 
     # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio([
+        "...",
+        "XXX",
+        "OO."]) == "X", "Xs wins"
+    assert checkio([
+        "OXO",
+        "XOX",
+        "OXO"]) == "O", "Os wins"
     assert checkio([
         "X.O",
         "XX.",
